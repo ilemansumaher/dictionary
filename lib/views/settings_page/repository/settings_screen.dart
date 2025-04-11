5908ade09ff21d62a4710486c5e4eb6a0956128c
ilemansumaher <sumaherileman@gmail.com>
2025-04-11 09:50:50 +0500

    ss

--- lib/views/settings_page/repository/settings_screen.dart
+++ lib/views/settings_page/repository/settings_screen.dart
@@ -5,6 +5,38 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0),color: Theme.of(context).colorScheme.secondary,),
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              
+              tileColor: 
+              title: Text("English"),
+              trailing: Radio(
+                value: false,
+                groupValue: null,
+                onChanged: (ValueKey) {},
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              tileColor: Theme.of(context).colorScheme.secondary,
+              title: Text("Turkmen"),
+              trailing: Radio(
+                value: false,
+                groupValue: null,
+                onChanged: (ValueKey) {},
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
